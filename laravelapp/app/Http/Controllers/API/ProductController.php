1e66a46cb39c8da2ff1ea0d81f66d51c94ad3720
Meynard Duran <pvt_mduran18@yahoo.com>
2021-12-28 09:20:40 +0800

    add delete and edit functions

--- laravelapp/app/Http/Controllers/API/ProductController.php
+++ laravelapp/app/Http/Controllers/API/ProductController.php
@@ -1,37 +1,83 @@
-<?php 
-    namespace App\Http\Controllers\API;
-    use App\Models\Product;
-    use Illuminate\Support\Facades\Validator;
+<?php
 
-    use App\Http\Controllers\Controller;
-    use Illuminate\Http\Request;
+namespace App\Http\Controllers\API;
+use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
 
-    class ProductController extends Controller {
-        public function index () {
-            $product = Product::all();
-            return response()->json(['status'=>200, "products"=>$product]);
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index () {
+        $product = Product::all();
+        return response()->json(['status'=>200, "products"=>$product]);
+    }
+    public function create (Request $request) {
+        $validator = Validator::make($request->all(),[
+            "name"=>"required", 
+            "description"=>"required", 
+            "price"=>"required",
+            "inStock"=>"required",
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status'=>422, "validate_err"=>$validator->errors()]);
+        }
+        else {
+            $product = New Product();
+            $product->name=$request->input('name');
+            $product->description=$request->input('description');
+            $product->price=$request->input('price');
+            $product->inStock=$request->input('inStock');
+            $product->save(); //query builder orm
+            return response()->json(['status'=>200, 'message'=>'product addded succesfully']);
         }
 
-        public function create (Request $request) {
-            $validator = Validator::make($request->all(),[
-                "name"=>"required",
-                "description"=>"required",
-                "price"=>"required",
-                "inStock"=>"required",
-            ]);
-            if ($validator->fails()) {
-                return response()->json(['status'=>422, "validate_error"=>$validator->errors()]);
-            }
-            else {
-                $product = New Product();
+    }
+    public function edit ($id) {
+        $product = Product::find($id);
+        if ($product) {
+            return response()->json(['status'=>200, "product"=>$product]);
+        }
+        else {
+            return response()->json(['status'=>404, 'message'=>'No product id found']);
+        }
+    }
+
+    public function update (Request $request, $id ) {
+        $validator = Validator::make($request->all(),[
+            "name"=>"required", 
+            "description"=>"required", 
+            "price"=>"required",
+            "inStock"=>"required",
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status'=>422, "validationError"=>$validator->errors()]);
+        }
+        else {
+            $product = Product:: find($id); //same to SELECT * FROM table Where id is
+            if ($product) {
                 $product->name = $request->input('name');
                 $product->description = $request->input('description');
                 $product->price = $request->input('price');
                 $product->inStock = $request->input('inStock');
-                $product->save();
-                return response()->json(['status'=>200, 'message'=>'product added succesfully']);
+                $product->update();
+                return response()->json(['status'=>200,  'message'=>'product updated succesfully']);
+            }
+            else {
+                return response()->json(['status'=>404, 'message'=>'No product id found']);
             }
         }
-        
     }
-?>
+    public function delete ($id) {
+        $product = Product::find($id);
+        if ($product) {
+            $product-> delete();
+            return response()->json(['status'=>200,'message'=>'Product deleted']);
+        }
+        else {
+            return response()->json(['status'=>404, 'message'=>'No product id found']);
+        }
+    }
+    
+}
--- reactapp/src/pages/Navbar.js
+++ reactapp/src/pages/Navbar.js
@@ -1,48 +1,31 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom';
-
-function Navbar() {
-   return (
-      <div>
-         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-               <a className="navbar-brand" href="#">
-                  Navbar
-               </a>
-               <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavAltMarkup"
-                  aria-controls="navbarNavAltMarkup"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-               >
-                  <span className="navbar-toggler-icon"></span>
-               </button>
-               <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div className ="navbar-nav">
-                     <li>
-                        <Link className="nav-link" to="/">
-                           Home
-                        </Link>
-                     </li>
-                     <li>
-                        <Link className="nav-link" to="/products">
-                           Product
-                        </Link>
-                     </li>
-                     <li>
-                        <Link className="nav-link" to="/addproduct">
-                           Add Product
-                        </Link>
-                     </li>
-                  </div>
-               </div>
-            </div>
-         </nav>
-      </div>
-   );
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className='nav-link' to='/'>Home</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <Link className='nav-link' to='/'>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link className='nav-link' to='/products'>product</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className='nav-link' to='/addproduct'>Add product</Link>
+                        </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar
